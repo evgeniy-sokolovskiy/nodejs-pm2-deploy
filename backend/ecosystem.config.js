ae61819ed7809a37e9c03e6c960b65202614ac01
evgeniy <sokol-rc@yandex.ru>
2024-10-07 21:33:57 +0300

    add pm2 config backend

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,26 @@
+require('dotenv').config({ path: '.env' });
+
+const {
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_PATH,
+  DEPLOY_REF = 'origin/main',
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'app1',
+    script: './app.js',
+  }],
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: 'https://github.com/evgeniy-sokolovskiy/nodejs-pm2-deploy',
+      path: DEPLOY_PATH,
+      'pre-deploy-local': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': 'cd backend && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
+    },
+  },
+};
