c6787a6bcd077e666a687c4e2dd9c8d5fe1ebe17
evgeniy <sokol-rc@yandex.ru>
2024-10-10 03:12:27 +0300

    fix api back 2

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -139,6 +139,6 @@ class Api {
   }
   }
   // Замените на адрес вашего бэкенда
-  const api = new Api('https://api.yamesto.nomorepartiesco.ru/');
+  const api = new Api('https://api.yamesto.nomorepartiesco.ru');
 
   export default api;
